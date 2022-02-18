b5afa0d2b535c56270ba6ed5ebd2a82218e94f58
Petar V Peshev <40143832+PetarVPeshev@users.noreply.github.com>
2022-02-18 13:25:34 +0100

    Created FTCurrent routine

---
+++ FTCurrent.m
@@ -0,0 +1,14 @@
+function [ Jx ] = FTCurrent( k0, er, KX, KY, L, W )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+%   Note: fix documentation of FTCurrent routine
+    % Create meshgrid
+    [ kx, ky ] = meshgrid(KX, KY);
+    % Calculate keq
+    keq = (kx + ky)/2;
+    % Calculate FT
+    T = sinc(ky * W / (2*pi));
+    L = 2*keq .* (cos(kx * L/2) - cos(keq * L/2)) ./...
+        ( (keq.^2 - kx.^2) .* sin(keq * L/2) );
+    Jx = T .* L;
+end
